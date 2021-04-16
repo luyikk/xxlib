d9e2730db60aaf48c3fbafb7f7e70d47f6c82c6a
yi lu <luyikk@126.com>
2021-04-16 15:19:25 +0800

    push

---
+++ test_template/Ref_test.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable CS0169
+using TemplateLibrary;
+
+
+namespace PKG.Ref
+{
+    [Desc("Test Ref"), TypeId(1222)]
+    public class TypeName
+    {
+        string name = "null";
+    }
+}
--- test_template/master.cs
+++ test_template/master.cs
@@ -2,10 +2,106 @@
 using TemplateLibrary;
 
 
+namespace PKG.Enum
+{
+    [Desc("Test Enum")]
+    public enum TypeId 
+    {
+        [Desc("A")]
+        a = 1,
+        [Desc("B")]
+        b = 2,
+        [Desc("C")]
+        c = 3
+    }
+}
+
+
+namespace PKG
+{
+    [Desc("Test Struct"),Struct]
+    public class TestStruct
+    {
+        [Desc("test enum")]
+        PKG.Enum.TypeId P =PKG.Enum.TypeId.a;
+        [Desc("test i32")]
+        int a = 55;
+        int b = 55;
+        [Desc("test f32")]
+        float c = 5;
+        [Desc("test f64")]
+        double x = 100.0;
+        [Desc("test string")]
+        string sb = "123123";
+        [Desc("test buff")]
+        byte[] buff;
+        [Desc("test btreemap")]
+        Dict<int, string> table;
+        [Desc("test hashmap")]
+        HashMap<int, string> hashtable;
+        [Desc("test hashset")]
+        HashSet<int> hashset;
+    }
+
+    [Desc("Test Struct"),TypeId(12),Compatible]
+    public class TestBase
+    {
+        float x = 1.0f;
+        float y = 2.0f;
+    }
+
+   [Desc("Test Struct"),TypeId(11)]
+    public class TestStruct2: TestBase
+    {
+        [Desc("test enum")]
+        PKG.Enum.TypeId P = PKG.Enum.TypeId.a;
+        [Desc("test i32")]
+        int a = 55;
+        int b = 55;
+        [Desc("test f32")]
+        float c = 5;
+        [Desc("test f64")]
+        double x = 100.0;
+        [Desc("test string")]
+        string sb = "123123";
+        [Desc("test buff")]
+        byte[] buff;
+        [Desc("test btreemap")]
+        Dict<int, string> table;
+        [Desc("test hashmap")]
+        HashMap<int, string> hashtable;
+        [Desc("test hashset")]
+        HashSet<int> hashset;
+        [Desc("test weak my")]
+        Weak<TestStruct2> weak_my;
+        [Desc("test sharedptr my")]
+        Shared<TestStruct2> shard_my;
+
+    }
+}
+
+namespace PKG.AA
+{
+    [Desc("Test Struct"), Struct,Compatible]
+    public class Test_B:TestStruct
+    {
+        [Desc("test enum")]
+        PKG.Enum.TypeId P = PKG.Enum.TypeId.b;
+        [Desc("test sharedptr my")]
+        Shared<TestStruct2> ptr;
+
+        [Desc("test sharedptr xy")]
+        Shared<TestBase> shard_xy;
+        [Desc("test ref")]
+        Shared<PKG.Ref.TypeName> ref_name;
+    }
+}
+
+
 
 namespace PKG.P
 {
-    [TypeId(12), Desc("Ponit"),Compatible]
+    [TypeId(122), Desc("Ponit"), Compatible]
     class Point
     {
         int X;
@@ -13,7 +109,7 @@ namespace PKG.P
         double? Z;
     }
 
-    [Struct,Desc("Ponit2")]
+    [Struct, Desc("Ponit2")]
     class Point2
     {
         float x;
@@ -21,7 +117,7 @@ namespace PKG.P
     }
 
     [Struct, Desc("Ponit3"), Compatible]
-    class Point3 :Point2
+    class Point3 : Point2
     {
         float z;
     }
@@ -40,7 +136,7 @@ namespace PKG.P
 namespace PKG
 {
 
-    [TypeId(11), Desc("Base")]
+    [TypeId(121), Desc("Base")]
     class Base
     {
         [Desc("S1")]
@@ -56,7 +152,7 @@ namespace PKG
     }
 
 
-    [TypeId(13), Desc("Foo"), Compatible]
+    [TypeId(123), Desc("Foo"), Compatible]
     class Foo : Base
     {
         int P1;
